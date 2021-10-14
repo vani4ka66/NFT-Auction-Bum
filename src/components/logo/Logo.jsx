a39dc5585ab07a43fd98033a1ab072cd6602ac3e
Vanya Dimitrova <vani4ka66@abv.bg>
2021-10-14 18:28:07 +0300

    Commit 3

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,19 +1,19 @@
-import Container from "@mui/material/Container";
-import classNames from "classnames";
-import { useState } from "react";
+import React from "react"
 import logo from "../../../public/images/logo.svg"
 import logoMuted from "../../../public/images/logo-muted.svg"
-// import logoEnum from "../enums/logoEnum";
 
-export default function Logo() {
-    const [type, setType] = useState('default')
 
-    return (
-        <div>
-            {type.default && <img src={logo} />}
-            {type.muted && <img src={logoMuted} />}
-            {console.log(type)}
-            
-        </div>
-  );
-}
+export default class Logo extends React.Component {
+    constructor(){
+        
+        this.type = 'default';
+    }
+
+    render() {
+      return <div>
+                {type === "default" && <img src={logo} />}
+                {type === "muted" && <img src={logoMuted} />}
+                {console.log(type)}
+            </div>
+    }
+  }
