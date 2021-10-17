c10c6ad3c4f935a0da9d381b139432275088faa5
Vanya Dimitrova <vani4ka66@abv.bg>
2021-10-17 14:47:45 +0300

    Commit 20

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,14 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import Container from "@mui/material/Container";
 import styles from "./Avatar.module.scss";
 import classNames from "classnames";
 
 
-export default function Avatar({size="90px", url, verified=false}) {
+export default function Avatar({size="90px", url="/images/avatar.png", verified=false}) {
+
+    const [width, setWidth] = useState(size)
+    const [height, setHeight] = useState(size)
+
+
   return (
     <Container className={classNames(styles.container)} maxWidth="xl">
-        <div width={size}  height={size}  className="avatar">
-            <img className="image" src="/images/avatar.png" alt="" />
+        <div width={width}  height={height}  className="avatar">
+            <img className="image" src={url} alt="" />
             {verified && <img className="badge" src="/images/verified.svg" alt="" />}
         </div>
       
