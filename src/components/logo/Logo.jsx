0afc724aa101ce5a673f1ea5a15e5aacb8917bfb
Vanya Dimitrova <vani4ka66@abv.bg>
2021-10-17 12:55:28 +0300

    Commit 13

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from "react"
 import classNames from "classnames";
-import logo from "../../../public/images/logo.svg"
-import logoMuted from "../../../public/images/logo-muted.svg"
 // import styles from "./Logo.module.scss";
 
-export default function Logo(type="default") {
+export default function Logo(props) {
 
   // const [typeA, setType] = useState('default')
 
   return (
     <div>
-    {/* {type === "default" && <img src="/images/logo-muted.svg" />} */}
-    <img src="/images/logo.svg" />
+      {/* {type === "default" && <img src="/images/logo-muted.svg" />} */}
+      
+      <img src="/images/logo.svg" />
 
-    {type === "muted" && <img src="/images/logo-muted.svg" />} 
+      {props.type === "muted" && <img src="/images/logo-muted.svg" />} 
      
 </div>
   );
