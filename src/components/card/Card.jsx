80b5cc1105a833fc277885f16cb1066af537237a
Vanya Dimitrova <vani4ka66@abv.bg>
2021-10-18 09:14:36 +0300

    Commit 39

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,5 @@
 // import Paper from "@mui/material/Paper";
-import React from "react"
+import React, { useState } from "react"
 import Container from "@mui/material/Container";
 import styles from "./Card.module.scss";
 import classNames from "classnames";
@@ -16,50 +16,39 @@ import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
-export default function Cardd({name, likes, mediaUrl, price, currency, user}) {
+export default function Card1({name, likes=0, mediaUrl, price, currency, user}) {
+    const [like, setLike] = useState(0)
+    user = {
+        verified: false,
+        avatarUrl: ""
+    }
 
   return (
     <div className={classNames(styles.wrapper)}>
-        <Container className={classNames(styles.container)} maxWidth="xl">
-            {/* <Avatar /> */}
-            <div className="card">
-                {/* <img className="media" src="/images/nft.jpg" alt="" /> */}
-                {/* <div className="title"></div> */}
-                {/* <div className="price">{price} {currency}</div> */}
-                {/* <div className="likes"></div> */}
-            </div>
-
-        </Container>
-
         <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={<Avatar />}
-        // title="Shrimp and Chorizo Paella"
-        verified="true"
-      />
-      <CardMedia
-        className="media"
-        component="img"
-        image="/images/nft.jpg"
-        alt="pic"
-      />
+            <CardHeader
+                avatar={<Avatar verified={user.verified} url={user.avatarUrl} />}
+                // title="Shrimp and Chorizo Paella"
+            />
+            <CardMedia
+                className={classNames(styles.media)}
+                component="img"
+                image="/images/nft.jpg"
+                alt="pic"
+            />
 
-      <CardActions >
-            <Typography variant="body2" color="text.secondary">
-                <div className="title"></div>
-                <div className="name">{name}</div>
-                <div className="price">{price}  {currency}</div>
-            </Typography>
-        
-            {/* <IconButton  aria-label="share">
-                <ShareIcon />
-            </IconButton> */}
+            <CardActions >
+                    <Typography variant="body2" color="text.secondary">
+                        <div className="title"></div>
+                        <div className="name">{name}</div>
+                        <div className="price">~{price}  {currency}</div>
+                    </Typography>
 
-            <IconButton aria-label="add to favorites">
-                <FavoriteIcon className="likes" /> {likes}
-            </IconButton>
-      </CardActions>
-    </Card>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon className="likes" /> {likes}
+                    </IconButton>
+            </CardActions>
+        </Card>
     </div>
   );
 }
