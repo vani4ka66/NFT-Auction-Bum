2fe09b62a39992b409ea517f6cde356ef49f62b0
Vanya Dimitrova <vani4ka66@abv.bg>
2021-10-19 10:44:35 +0300

    Commit 52

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -7,48 +7,7 @@ import Select from '@mui/material/Select';
 import { Grid } from "@mui/material";
 import { MenuItem } from "@mui/material";
 
-export default function Avatar({size=90, url="/images/avatar.png", verified=false}) {
-
- 
-    const card1 = {
-        name: "Ivy",
-        user :{
-            avatarUrl: "images/avatr.png",
-            verified: true
-        },
-        price: 1,
-        currency: "ETH"
-    }
-
-    const card2 = {
-        name: "Judie",
-        user :{
-            avatarUrl: "images/avatr.png",
-            verified: true
-        },
-        price: 2.3,
-        currency: "ETH"
-    }
-
-    const card3 = {
-        name: "Juniper",
-        user :{
-            avatarUrl: "images/avatr.png",
-            verified: true
-        },
-        price: 5,
-        currency: "ETH"
-    }
-
-    const card4 = {
-        name: "Maple",
-        user :{
-            avatarUrl: "images/avatr.png",
-            verified: true
-        },
-        price: 10,
-        currency: "ETH"
-    }
+export default function Trending({cards=[]}) {
 
   return (
     <Container className={classNames(styles.container)} maxWidth="xl">
@@ -64,22 +23,12 @@ export default function Avatar({size=90, url="/images/avatar.png", verified=fals
         </Grid>
        
         <Grid container spacing={2}>
-            <Grid item xs={3}>
-                <Card {...card1} />
-            </Grid >
-            <Grid item xs={3}>
-                <Card {...card2} />
-            </Grid >
-            <Grid item xs={3}>
-                <Card {...card3} />
-            </Grid >
-            <Grid item xs={3}>
-                <Card {...card4} />
-            </Grid >
+            {cards.map(element => {
+                <Grid item xs={3}>
+                    <Card {...element} />
+                </Grid >
+            })}
         </Grid>
-            {/* <img width={size} height={size} className={classNames(styles.image)} src={url} alt="" /> */}
-      
-      
     </Container>
   );
 }
