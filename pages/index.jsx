4088f3692fe817a307e8c562db4f57d914d84932
Vanya Dimitrova <vani4ka66@abv.bg>
2021-10-28 09:30:07 +0300

    Commit 83

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 
-import CollectorsColumn from "../src/components/collectorsColumn/CollectorsColumn";
+import CollectorColumn from "../src/components/collectorColumn/CollectorColumn";
 import How from "../src/components/how/How";
 
 
 export default function Index() {
-  return <CollectorsColumn />;
+  return <CollectorColumn />;
 }
