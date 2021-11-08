ea98d8193e486747f9184c9f3613ca433fca4f33
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-08 18:44:10 +0200

    Commit 105

--- src/components/product/ProductInfoLikes.jsx
+++ src/components/product/ProductInfoLikes.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import styles from "./ProductInfoLikes.module.scss";
 import classNames from "classnames";
 import { Chip } from '@mui/material';
+import millify from "millify";
 
 
 
@@ -9,7 +10,7 @@ export default function ProductInfoLikes({amount=0}) {
        
   return (
       <div className="ProductInfoLikes_product-info-likes">
-        <Chip label={likes} className="ProductInfoLikes_likes" />
+        <Chip label={millify(likes)} className="ProductInfoLikes_likes" />
       </div>
   );
 }
