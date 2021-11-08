e6d7c42926638cfed8cc0e0843d85f52d9b0604c
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-08 20:05:19 +0200

    Commit 110

--- src/components/product/ProductInfoCreator.jsx
+++ src/components/product/ProductInfoCreator.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import styles from "./ProductInfoCreator.module.scss";
-'@use "../../styles/colors'
 import classNames from "classnames";
 import { Chip } from '@mui/material';
 import User from "../user/User";
@@ -11,7 +10,7 @@ export default function ProductInfoCreator({name, avatar, verified=false}) {
        
   return (
       <div className="ProductInfoCreator_product-info-creator">
-        <label className="title"></label>
+        <label className="title">Creator</label>
         <div className="user-container">
             <User name={name} avatar={avatar} verified={verified} />
         </div>
