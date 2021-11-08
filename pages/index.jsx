cedc8c77e400933dab90d27873c421da42806193
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-08 16:18:08 +0200

    Commit 92

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,10 @@
 
-import CollectorColumn from "../src/components/collectors/CollectorColumn";
+import TopCollectors from "../src/components/collectors/TopCollectors";
 import Collector from "../src/components/collectors/Collector";
 
 
 
 
 export default function Index() {
-  return <Collector />;
+  return <TopCollectors />;
 }
---
+++ src/components/collectors/TopCollectors.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react"
+import Container from "@mui/material/Container";
+import styles from "./TopCollectors.module.scss";
+import classNames from "classnames";
+import CollectorColums from "../collectors/CollectorColumn"
+import Select from '@mui/material/Select';
+import { Grid } from "@mui/material";
+import { MenuItem } from "@mui/material";
+
+export default function TopCollectors({collectors=[]}) {
+
+    const collectorss = [
+        {
+            name: "aaaaa",
+            nftCount: 1,
+            avatar: "",
+            verified: true
+        },
+        {
+            name: "bbbbb",
+            nftCount: 2,
+            avatar: "",
+            verified: true
+        },
+        {
+            name: "cccccc",
+            nftCount: 3,
+            avatar: "",
+            verified: true
+        },
+        {
+            name: "ddddd",
+            nftCount: 4,
+            avatar: "",
+            verified: true
+        },
+        {
+            name: "fffff",
+            nftCount: 5,
+            avatar: "",
+            verified: true
+        },
+    ]
+
+    // var asasas = _.chunk(collectorss, 3);
+
+  return (
+    <Container className={classNames(styles.container)} maxWidth="xl">
+        <Grid container spacing={2}>
+            <Grid item xs={9}>
+                <h1>Top Collectors</h1>
+            </Grid >
+            <Grid item xs={3}>
+              <Select className={classNames(styles.select)} value="This week" label="Age">
+                <MenuItem value={10}>Ten</MenuItem>
+              </Select>
+            </Grid >
+        </Grid>
+       
+        <Grid container spacing={2}>
+            {(collectors.map(collector => {
+               return   <Grid item xs={3}>
+                            <CollectorColums id={collector.id} {...collector} />
+                            <div>djsdskdj</div>
+                        </Grid >
+            }))}
+        </Grid>
+    </Container>
+  );
+}
