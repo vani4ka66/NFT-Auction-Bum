e82a33e6f1ee0623f073fde20f5fd530b3bec56a
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-10 10:39:35 +0200

    Commit 121

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -7,7 +7,7 @@ export default function ProductInfoTimer({timeEnd,onTimeEnd}) {
        
   return (
       <div className="ProductInfoTimer_product-info-timer">
-            {onTimeEnd && <div className="timer">
+            {timeEnd !== null && <div className="timer">
                             <label className="title">Ends in</label>
                             <Countdown className={classNames(styles.ProductInfoTimer_active)} timeEnd={timeEnd} onTimeEnd={onTimeEnd} />
                         </div>
