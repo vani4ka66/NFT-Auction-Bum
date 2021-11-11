1072ed489cec786f9d4704cd922dbdd3c15f69be
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-11 08:47:25 +0200

    Commit 128

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react"
 import styles from "./ProductInfoTimer.module.scss";
 import classNames from "classnames";
 import Countdown from 'react-countdown';
+import { style } from "@mui/material/node_modules/@mui/system";
 
 export default function ProductInfoTimer({timeEnd,onTimeEnd}) {
        
   return (
-      <div className={classNames(styles["ProductInfoTimer_product-info-timer"], {[styles["active"]]: timeEnd})}>
+      // <div className={classNames( {[styles["active"]]: timeEnd}, styles["ProductInfoTimer_product-info-timer"])}>
+      <div className={"ProductInfoTimer_product-info-timer " + (timeEnd ? [styles["active"]] : '')}>
+
         <label className="title">Ends in</label>
            <div className="timer">
               {timeEnd && <Countdown date={timeEnd} onStop={onTimeEnd} />}
