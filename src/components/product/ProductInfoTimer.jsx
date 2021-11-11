80467067bab8aad3ebdd78a4b479b162cb5f2fe7
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-11 08:30:23 +0200

    Commit 127

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -6,14 +6,11 @@ import Countdown from 'react-countdown';
 export default function ProductInfoTimer({timeEnd,onTimeEnd}) {
        
   return (
-    <div className={classNames(styles.container)}>
       <div className={classNames(styles["ProductInfoTimer_product-info-timer"], {[styles["active"]]: timeEnd})}>
         <label className="title">Ends in</label>
            <div className="timer">
               {timeEnd && <Countdown date={timeEnd} onStop={onTimeEnd} />}
           </div>
       </div>
-    </div>
-     
   );
 }
