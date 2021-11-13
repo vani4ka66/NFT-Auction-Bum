d76cf4271f48475e2e5f900390b689762478961b
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-13 12:06:17 +0200

    Commit 138

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -2,33 +2,37 @@ import React, { useState } from "react"
 import styles from "./ProductInfo.module.scss";
 import classNames from "classnames";
 import User from "../user/User";
-import { Container, Tab, TableRow } from "@mui/material";
-// import { TabContext } from '@mui/lab';
+import {  Tab, TableRow } from "@mui/material";
 import TabContext from '@mui/lab/TabContext';
 import { parseISO, formatDistance } from 'date-fns'
 
 
 export default function ProductTabs({text, bids=[]}) {
+
+  const bidss = [
+    {
+      id: '1',
+      name: 'ldskdskd',
+      lastName: 'bbbbbb',
+      date: Date.now()
+    }
+  ]
        
   return (
         <div className="ProductTabs_produst-tabs" maxWidth="xl">
-              <TabContext className="tab-details" value={text}>
+            <User />
+              <TabContext className="tab-details">
                   <Tab label="Details" className="ProductsTab_tab-details" />
                   <Tab label="Bids" className="ProductsTab_tab-bids" />
                 </TabContext>
-            
-
-              {bids.map((bid, i) => {
-                <div>
-                  <TableRow className={`table-row-${i}`}   value={formatDistance({bid})} />
-                  {/* <Tab label="Bids" className="tab-bids" value={bid} /> */}
-                  {formatDistance({bid})}
-                </div>
-              })}
-              
-           
-            <User />
-
+          
+              {(bidss.map((bid, i) => {
+                    return  <div>
+                               <TableRow className={`table-row-${i}`} />
+                               {/* {formatDistance(parseISO(bid.date), 'yyyy-MM-dd')} */}
+                              {/* {formatDistance(parseISO(bid.date))} */}
+                            </div >
+              }))}
         </div>
   )
 }
