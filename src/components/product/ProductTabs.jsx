af419e2d7fe27f336b837bd0d3653e245990d523
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-13 11:42:13 +0200

    Commit 137

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -3,26 +3,32 @@ import styles from "./ProductInfo.module.scss";
 import classNames from "classnames";
 import User from "../user/User";
 import { Container, Tab, TableRow } from "@mui/material";
-import { TabContext } from '@mui/lab';
+// import { TabContext } from '@mui/lab';
+import TabContext from '@mui/lab/TabContext';
+import { parseISO, formatDistance } from 'date-fns'
 
 
 export default function ProductTabs({text, bids=[]}) {
        
   return (
-        <Container className={styles["product-tabs"]} maxWidth="xl">
+        <div className="ProductTabs_produst-tabs" maxWidth="xl">
+              <TabContext className="tab-details" value={text}>
+                  <Tab label="Details" className="ProductsTab_tab-details" />
+                  <Tab label="Bids" className="ProductsTab_tab-bids" />
+                </TabContext>
+            
 
-            <TabContext label="Details" className="tab-details" value={text} />
-           
-           
-            {bids.map((bid, i) => {
-              <div>
-                <TableRow className={`table-row-${i}`} />
-                <TabContext label="Bids" className="tab-bids" value={bid} />
-               </div>
-            })}
+              {bids.map((bid, i) => {
+                <div>
+                  <TableRow className={`table-row-${i}`}   value={formatDistance({bid})} />
+                  {/* <Tab label="Bids" className="tab-bids" value={bid} /> */}
+                  {formatDistance({bid})}
+                </div>
+              })}
+              
            
             <User />
 
-        </Container>
+        </div>
   )
 }
