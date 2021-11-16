ad4a8301c12cf04c762491c0e0e04ed19f209418
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-16 07:52:00 +0200

    Commit 145

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -6,30 +6,30 @@ import {  Tab, TableBody, TableCell, TableRow } from "@mui/material";
 import TabContext from '@mui/lab/TabContext';
 import { parseISO, formatDistance } from 'date-fns'
 
-
 export default function ProductTabs({text, bids=[]}) {
        
   return (
         <div className="ProductTabs_product-tabs" maxWidth="xl">
-              <TabContext className="tab-details">
+            <TabContext className="tab-details">
                   <Tab label="Details" className="ProductsTab_tab-details" />
                   <Tab label="Bids" className="ProductsTab_tab-bids" />
-                </TabContext>
-          
-              {(bids.map((bid, i) => {
-                    return  <div>
-                              <Table>
-                                    <TableBody>
+            </TabContext>
+
+            <Table>
+                  <TableBody>
+                        {(bids.map((bid, i) => {
+                              return  <div>
                                           <TableRow className={`table-row-${i}`}>
                                                 {/* {formatDistance(parseISO(bid.date), 'yyyy-MM-dd')} */}
                                                 <TableCell>
                                                       <User {...bid} />
                                                 </TableCell>
                                           </TableRow>
-                                    </TableBody>
-                              </Table>
-                            </div >
-              }))}
+                                    </div >
+                        }))}
+
+                  </TableBody>
+            </Table>
         </div>
   )
 }
