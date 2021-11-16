2a2a1ab2c451c5cb218b501b1722513db1f635d8
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-16 09:00:23 +0200

    Commit 151

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -2,37 +2,29 @@ import React, { useState } from "react"
 import styles from "./ProductInfo.module.scss";
 import classNames from "classnames";
 import User from "../user/User";
-import {  Tab, TabContext, TableCell, TableRow } from "@mui/material";
+import {  Tab, TableRow } from "@mui/material";
+import TabContext from '@mui/lab/TabContext';
 import { parseISO, formatDistance } from 'date-fns'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-// import TableRow from '@mui/material/TableRow';
+
 
 export default function ProductTabs({text, bids=[]}) {
        
   return (
         <div className="ProductTabs_product-tabs" maxWidth="xl">
-            <TabContext className="tab-details">
+              <TabContext className="tab-details">
                   <Tab label="Details" className="ProductsTab_tab-details" />
                   <Tab label="Bids" className="ProductsTab_tab-bids" />
-            </TabContext>
-
-            {/* <Table> */}
-                  {/* <TableBody> */}
-                        {(bids.map((bid, i) => {
-                              return  <div>
-                                          <TableRow className={`table-row-${i}`}>
-                                                {/* {formatDistance(parseISO(bid.date), 'yyyy-MM-dd')} */}
-                                                {/* <TableCell> */}
-                                                      <User {...bid} />
-                                                {/* </TableCell> */}
-                                               
-                                          </TableRow>
-                                    </div >
-                        }))}
-
-                  {/* </TableBody> */}
-            {/* </Table> */}
+                </TabContext>
+          
+              {(bids.map((bid, i) => {
+                    return  <div>
+                               <TableRow className={`table-row-${i}`}>
+                               {/* {formatDistance(parseISO(bid.date), 'yyyy-MM-dd')} */}
+                              
+                                  <User {...bid} />
+                               </TableRow>
+                            </div >
+              }))}
         </div>
   )
 }
