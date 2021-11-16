8a6c21e72dbc5a2deede8ee0c76896945d65f4c2
Vanya Dimitrova <vani4ka66@abv.bg>
2021-11-16 07:57:39 +0200

    Commit 146

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -2,9 +2,15 @@ import React, { useState } from "react"
 import styles from "./ProductInfo.module.scss";
 import classNames from "classnames";
 import User from "../user/User";
-import {  Tab, TableBody, TableCell, TableRow } from "@mui/material";
+import {  Tab, } from "@mui/material";
 import TabContext from '@mui/lab/TabContext';
 import { parseISO, formatDistance } from 'date-fns'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 export default function ProductTabs({text, bids=[]}) {
        
@@ -15,21 +21,27 @@ export default function ProductTabs({text, bids=[]}) {
                   <Tab label="Bids" className="ProductsTab_tab-bids" />
             </TabContext>
 
-            <Table>
-                  <TableBody>
+            {/* <Table> */}
+                  {/* <TableBody> */}
                         {(bids.map((bid, i) => {
                               return  <div>
                                           <TableRow className={`table-row-${i}`}>
                                                 {/* {formatDistance(parseISO(bid.date), 'yyyy-MM-dd')} */}
                                                 <TableCell>
-                                                      <User {...bid} />
+                                                      <User />
+                                                </TableCell>
+                                                <TableCell>
+                                                      {bid.amount}
+                                                </TableCell>
+                                                <TableCell>
+                                                      {bid.date}
                                                 </TableCell>
                                           </TableRow>
                                     </div >
                         }))}
 
-                  </TableBody>
-            </Table>
+                  {/* </TableBody> */}
+            {/* </Table> */}
         </div>
   )
 }
